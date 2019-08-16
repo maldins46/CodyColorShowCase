36d7f2a09c2e4748360d815166476584ad64416b
Riccardo <riccardo.maldini@gmail.com>
2019-08-16 16:04:16 +0200

    patch 3.0.2: service worker aggiornato

--- src/contents/service-worker.js
+++ src/contents/service-worker.js
@@ -1,33 +1,12 @@
-// codice da 'iniettare' in workbox, nel caso in cui si voglia andare a modificare il sistema di
-// caching predefinito
+/* File per personalizzare ulteriormente il service worker generato con workbox. E' qua possibile aggiungere del
+ * codice da 'iniettare' nel service worker, nel caso in cui si vogliano aggiungere delle funzioni allo stesso, oltre
+ * alla funzionalita' di caching predefinita.
+ */
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-/*
-// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
-workbox.routing.registerRoute(
-    /^https:\/\/fonts\.googleapis\.com/,
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'google-fonts-stylesheets',
-    })
-);
+// Place code to inject here
 
-// Cache the underlying font files with a cache-first strategy for 1 year.
-workbox.routing.registerRoute(
-    /^https:\/\/fonts\.gstatic\.com/,
-    new workbox.strategies.CacheFirst({
-        cacheName: 'google-fonts-webfonts',
-        plugins: [
-            new workbox.cacheableResponse.Plugin({
-                statuses: [0, 200],
-            }),
-            new workbox.expiration.Plugin({
-                maxAgeSeconds: 60 * 60 * 24 * 365,
-                maxEntries: 30,
-            }),
-        ],
-    })
-);*/
-
+// pulisci la cache quando giunta a scadenza
 workbox.precaching.precacheAndRoute([
   {
     "url": "audio/countdown.mp3",
@@ -487,7 +466,7 @@ workbox.precaching.precacheAndRoute([
   },
   {
     "url": "js/sessionHandler.js",
-    "revision": "747fe583423ac5dee57c40f551b4b31b"
+    "revision": "1da9ebcf830bb6b9957a878a46ce4e40"
   },
   {
     "url": "js/translationHandler.js",
