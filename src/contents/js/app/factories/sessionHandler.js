90481fd52422dd40ca421e4c77146ca4d9f484ee
riccardomaldini <riccardo.maldini@gmail.com>
2020-06-17 23:38:22 +0200

    Switched ranking in royale aftermatch, modified their css for better viewing, switched to version 3.3.3

--- src/contents/js/app/factories/sessionHandler.js
+++ src/contents/js/app/factories/sessionHandler.js
@@ -6,7 +6,7 @@ angular.module('codyColor').factory("sessionHandler", function() {
     let sessionHandler = {};
     let isSessionValid;
 
-    const wallVersion = '3.3.2';
+    const wallVersion = '3.3.3';
 
     // informazioni sul sistema, aggiornate automaticamente dal server non appena connessi
     let generalInfo = {
